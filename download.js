59d1964749acccd64558ac85b05eb53b1cadb2e1
realscottpilgrim <suziejoanmurray@gmail.com>
2020-10-14 16:47:05 +0100

    Create download.js

---
+++ download.js
@@ -0,0 +1,26 @@
+function saveTextAsFile()
+{
+    //inputTextToSave--> the text area from which the text to save is
+    //taken from
+    var textToSave = document.getElementById("pagetitle").value;
+    var textToSaveAsBlob = new Blob([textToSave], {type:"text/plain"});
+    var textToSaveAsURL = window.URL.createObjectURL(textToSaveAsBlob);
+    var fileNameToSaveAs = "PCS.txt"; 
+    
+    var downloadLink = document.createElement("a");
+    downloadLink.download = fileNameToSaveAs;
+    downloadLink.innerHTML = "Download File";
+    downloadLink.href = textToSaveAsURL;
+    downloadLink.onclick = destroyClickedElement;
+    downloadLink.style.display = "none";
+    document.body.appendChild(downloadLink);
+
+    downloadLink.click();
+}
+
+function destroyClickedElement(event)
+{
+    document.body.removeChild(event.target);
+}
+
+
