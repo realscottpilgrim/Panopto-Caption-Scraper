96fddfa9566470a82364dc836e9822755738f9e0
realscottpilgrim <suziejoanmurray@gmail.com>
2020-10-14 17:01:47 +0100

    Update and rename download.js to accessCaptions.js

--- accessCaptions.js
+++ accessCaptions.js
@@ -1,9 +1,11 @@
+var captions = document.getElementById("pagetitle").value;
+
 function saveTextAsFile()
 {
     //inputTextToSave--> the text area from which the text to save is
     //taken from
-    var textToSave = document.getElementById("pagetitle").value;
-    var textToSaveAsBlob = new Blob([textToSave], {type:"text/plain"});
+    var textToSave = captions;
+    var textToSaveAsBlob = new Blob([textToSave], {type:"text/plain"}); //google chrome not enjoying this
     var textToSaveAsURL = window.URL.createObjectURL(textToSaveAsBlob);
     var fileNameToSaveAs = "PCS.txt"; 
     
@@ -23,4 +25,10 @@ function destroyClickedElement(event)
     document.body.removeChild(event.target);
 }
 
+function copyTextToClipboard(){
+    var copyText = captions;
+    copytext.select();
+    document.execCommand("copy");
+    alert("Copied to clipboard)");
+}
 
