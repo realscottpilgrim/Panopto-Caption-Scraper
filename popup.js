f140b22f66b4b72a42a62f59ebb1187a0908e112
realscottpilgrim <suziejoanmurray@gmail.com>
2020-10-15 12:42:54 +0100

    Update popup.js

--- popup.js
+++ popup.js
@@ -17,31 +17,6 @@ chrome.runtime.onMessage.addListener(function (jsonArray) {
 
 
 
-function saveTextAsFile()
-{
-    //inputTextToSave--> the text area from which the text to save is
-    //taken from
-    var textToSave = captionText;
-    var textToSaveAsBlob = new Blob([textToSave], {type:"text/plain"}); //google chrome not enjoying this
-    var textToSaveAsURL = window.URL.createObjectURL(textToSaveAsBlob);
-    var fileNameToSaveAs = "PCS.txt"; 
-    
-    var downloadLink = document.createElement("a");
-    downloadLink.download = fileNameToSaveAs;
-    downloadLink.innerHTML = "Download File";
-    downloadLink.href = textToSaveAsURL;
-    downloadLink.onclick = destroyClickedElement;
-    downloadLink.style.display = "none";
-    document.body.appendChild(downloadLink);
-
-    downloadLink.click();
-}
-
-function destroyClickedElement(event)
-{
-    document.body.removeChild(event.target);
-}
-
 function copyTextToClipboard(){
     var copyText = captionText;
     copytext.select();
